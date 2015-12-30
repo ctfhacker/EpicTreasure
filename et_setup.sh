7fe5f28748aed3d201e0fc6a9e206a182bfb4e42
thebarbershopper <cld251@gmail.com>
2015-12-30 17:32:32 -0600

    Install r2pipe

--- et_setup.sh
+++ et_setup.sh
@@ -150,3 +150,6 @@ make -i
 sudo apt-get build-dep python-imaging
 sudo apt-get install libjpeg8 libjpeg62-dev libfreetype6 libfreetype6-dev
 sudo pip install Pillow
+
+# Install r2pipe
+sudo pip install r2pipe
