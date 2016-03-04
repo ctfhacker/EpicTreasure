828ac5765804b2da2333c62a3cbceff35dec9bbd
thebarbershopper <cld251@gmail.com>
2016-03-04 11:31:10 -0600

    Restore dotfiles in config

--- et_setup.sh
+++ et_setup.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
+HOMEDIR=/home/vagrant
+
 # Updates
 sudo apt-get -y update
-sudo apt-get -y upgrade
 
 sudo apt-get -y install python3-pip
 sudo apt-get -y install tmux
@@ -11,6 +12,7 @@ sudo apt-get -y install unzip
 sudo apt-get -y install foremost
 sudo apt-get -y install ipython
 
+"""
 # QEMU with MIPS/ARM - http://reverseengineering.stackexchange.com/questions/8829/cross-debugging-for-mips-elf-with-qemu-toolchain
 sudo apt-get -y install qemu qemu-user qemu-user-static
 sudo apt-get -y install 'binfmt*'
@@ -29,12 +31,13 @@ ln -s /usr/mipsel-linux-gnu /etc/qemu-binfmt/mipsel
 ln -s /usr/arm-linux-gnueabihf /etc/qemu-binfmt/arm
 rm /etc/apt/sources.list.d/emdebian.list
 sudo apt-get update
+"""
 
 # Install Binjitsu
 sudo apt-get -y install python2.7 python-pip python-dev git
 sudo pip install --upgrade git+https://github.com/binjitsu/binjitsu.git
 
-cd
+cd $HOMEDIR
 mkdir tools
 cd tools
 
@@ -54,12 +57,13 @@ sudo python3 setup.py install # Ubuntu 14.04+, GDB uses Python3
 sudo pip3 install pycparser # Use pip3 for Python3
 
 # Install radare2
+cd ~
 git clone https://github.com/radare/radare2
 cd radare2
 ./sys/install.sh
 
 # Install binwalk
-cd 
+cd ~
 git clone https://github.com/devttys0/binwalk
 cd binwalk
 sudo python setup.py install
@@ -82,20 +86,14 @@ sudo pip2 uninstall capstone -y
 cd ~/tools/capstone/bindings/python
 sudo python setup.py install
 
-# Personal config
-sudo sudo apt-get -y install stow
-cd /home/vagrant
-rm .bashrc
-git clone https://github.com/thebarbershopper/dotfiles
-
 # Install Angr
-cd /home/vagrant
+cd $HOMEDIR
 sudo apt-get -y install python-dev libffi-dev build-essential virtualenvwrapper
 sudo pip install angr --upgrade
 
 # Install american-fuzzy-lop
 sudo apt-get -y install clang llvm
-cd ~/tools
+cd $HOMEDIR/tools
 wget --quiet http://lcamtuf.coredump.cx/afl/releases/afl-latest.tgz
 tar -xzvf afl-latest.tgz
 rm afl-latest.tgz
@@ -126,34 +124,23 @@ mv apktool /bin/
 chmod 755 /bin/apktool
 chmod 755 /bin/apktool.jar
 
-# Install preeny
-git clone --depth 1 https://github.com/zardus/preeny
-PATH=$PWD/../crosstool/bin:$PATH
-
-cd preeny
-for i in ../../crosstool/bin/*-gcc
-do
-    t=$(basename $i)
-    CC=$t make -j $(nproc) -i
-done
-PLATFORM=-m32 setarch i686 make -i
-mv x86_64-linux-gnu i686-linux-gnu
-make -i
-
 # Install Pillow
 sudo apt-get build-dep python-imaging
-sudo apt-get install libjpeg8 libjpeg62-dev libfreetype6 libfreetype6-dev
+sudo apt-get -y install libjpeg8 libjpeg62-dev libfreetype6 libfreetype6-dev
 sudo pip install Pillow
 
 # Install r2pipe
 sudo pip install r2pipe
 
-# Install angr-dev
-cd ~/tools
-git clone https://github.com/angr/angr-dev
-cd angr-dev
-
 # Install ROPGadget
 git clone https://github.com/JonathanSalwan/ROPgadget
 cd ROPgadget
 sudo python setup.py install
+
+# Personal config
+sudo sudo apt-get -y install stow
+cd /home/vagrant
+rm .bashrc
+git clone https://github.com/ctfhacker/dotfiles
+cd dotfiles
+./install.sh
