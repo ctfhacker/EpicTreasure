e338f868e5b3d1ae7a4c0105e229fdc8b02ed147
thebarbershopper <cld251@gmail.com>
2016-04-14 12:58:29 -0500

    always try to build before running tests

--- docker/run_tests.sh
+++ docker/run_tests.sh
@@ -1,4 +1,6 @@
 #!/bin/sh
+eval $(docker-machine env default)
+docker build -t ctfhacker/epictreasure .
 docker run -v /Users/coryduplantis/workspace/epictreasure/docker/tests:/tmp/tests --rm ctfhacker/epictreasure /tmp/tests/run.sh
 if [ $? -eq 0 ]
 then
