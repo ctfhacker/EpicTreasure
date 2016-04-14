a73488d4c2363c5b1ddec2c181d3872c338c4c7f
thebarbershopper <cld251@gmail.com>
2016-04-14 12:04:52 -0500

    add pwndbg and radare test

--- docker/run_tests.sh
+++ docker/run_tests.sh
@@ -1,2 +1,6 @@
 #!/bin/sh
 docker run -v /Users/coryduplantis/workspace/epictreasure/docker/tests:/tmp/tests --rm ctfhacker/epictreasure /tmp/tests/run.sh
+if [ $? -eq 0 ]
+then
+    docker push ctfhacker/epictreasure
+fi
