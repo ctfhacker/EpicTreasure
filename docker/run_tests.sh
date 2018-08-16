84c7a946394652427d32049072700bf406c48327
thebarbershopper <cld251@gmail.com>
2018-08-15 20:04:59 -0500

    Remove old vagrant files - pure docker now

--- docker/run_tests.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-eval $(docker-machine env default)
-docker build -t ctfhacker/epictreasure .
-docker run -v /Users/coryduplantis/workspace/epictreasure/docker/tests:/tmp/tests --rm ctfhacker/epictreasure /tmp/tests/run.sh
-if [ $? -eq 0 ]
-then
-    docker push ctfhacker/epictreasure
-fi
